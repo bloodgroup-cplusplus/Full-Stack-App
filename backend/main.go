b579bbed84e62fbb5662127f3acb4f756e23d224
bloodgroup-cplusplus <81256027+bloodgroup-cplusplus@users.noreply.github.com>
2024-05-25 20:56:28 +0530

    building middlewares for backend server

--- backend/main.go
+++ backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"database/sql"
 	"log"
+	"net/http"
 	"os"
 
 	"github.com/gorilla/mux"
@@ -38,5 +39,31 @@ func main() {
 	router.HandleFunc("/api/go/users/{id}", getUser(db)).Methods("GET")
 	router.HandleFunc("/api/go/users/{id}", getUser(db)).Methods("PUT")
 	router.HandleFunc("/api/go/users/{id}", getUser(db)).Methods("DELETE")
+
+	// wrap the router with CORS and JSON content type middleware
+
+	enhancedRouter := enableCORS(jsonContentTypeMiddleware(router))
+
+	// start server
+	log.Fatal(http.ListenAndServe(":8000", enhancedRouter))
+}
+
+func enableCORS(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		// Set CORS headers
+		w.Header().Set("Access-Control-Allow-Origin", "*") // Allow any origin
+		w.Header().Set("Access-Control-Allow-Methods", "GET,POST,PUT1,DELETE,OPTIONS")
+		w.Header().Set("Access-Control-Allow-Headers", "Content-Type")
+
+		// Checck if reuest for CORS preflight
+		if r.Method == "OPTIONS" {
+			w.WriteHeader(http.StatusOK)
+			return
+		}
+
+		// Pass down the request to the next middleware (or final handler)
+
+		next.ServeHTTP(w, r)
+	})
 
 }
