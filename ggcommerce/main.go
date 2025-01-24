b93942a87e00e023c5d34ff6212df2952c54aa73
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2025-01-24 20:33:33 +0530

    adding weavebox

--- ggcommerce/main.go
+++ ggcommerce/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/anthdm/weavebox"
+)
 
 func main() {
+	app := weavebox.New()
+	app.Get("/product",func(*weavebox.Context) error {return nil})
+	app.Serve(3001)
 	fmt.Println("It's all good")
 }
