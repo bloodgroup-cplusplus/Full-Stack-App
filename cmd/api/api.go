d3366af59a9dac08615f501e27cf2e5a2eb57fff
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2025-02-17 10:17:29 +0530

    adding new store variables

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -5,12 +5,14 @@ import (
 	"net/http"
 	"time"
 
+	"github.com/bloodgroup-cplusplus/Full-Stack-App/internal/store"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 )
 
 type application struct {
 	config config
+	store store.Storage
 }
 
 type config struct {
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -4,6 +4,7 @@ import (
 	"log"
 
 	"github.com/bloodgroup-cplusplus/Full-Stack-App/internal/env"
+	"github.com/bloodgroup-cplusplus/Full-Stack-App/internal/store"
 )
 
 
@@ -12,11 +13,17 @@ func main() {
 	cfg := config{
 		addr: env.GetString("ADDR",":8059"),
 	}
+	store := store.NewStorage(nil)
 
 	app := &application{
 		config: cfg,
+		store:store,
+
 
 	}
+
+	
+	log.Fatal(store)
 
 	mux := app.mount()
 	log.Fatal(app.run(mux))
