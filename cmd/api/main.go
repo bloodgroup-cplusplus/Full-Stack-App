3688b42ffbb4954544f0a4470a032cab61b76214
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2025-02-17 11:05:40 +0530

    adding params to config variables

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -12,6 +12,12 @@ func main() {
 
 	cfg := config{
 		addr: env.GetString("ADDR",":8059"),
+		db: dbConfig{
+			addr : env.GetString("DB_ADDR","postgres://user:adminpassword@loaclhost/social?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS",30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS",30),
+			maxIdleTime: env.GetString("DB_MAX_IDLE_TIME","15min"),
+		},
 	}
 	store := store.NewStorage(nil)
 
