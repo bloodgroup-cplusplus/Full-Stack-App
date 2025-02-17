25bab2e9a84060fc66e733380c9e2a7fa4d7150f
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2025-02-17 09:10:16 +0530

    adding environment files

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,18 +1,23 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/bloodgroup-cplusplus/Full-Stack-App/internal/env"
+)
+
 
 func main() {
 
 	cfg := config{
-		addr: ":8070",
+		addr: env.GetString("ADDR",":8059"),
 	}
 
 	app := &application{
 		config: cfg,
 
 	}
-	
+
 	mux := app.mount()
 	log.Fatal(app.run(mux))
 
