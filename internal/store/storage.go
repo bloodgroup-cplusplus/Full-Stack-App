5d0430766b1c48c7a27544206ff3d0e6a923cea2
bloodgroup-cplusplus <tucater10896601@protonmail.com>
2025-02-17 09:52:49 +0530

    adding users and posts

---
+++ internal/store/storage.go
@@ -0,0 +1,24 @@
+package store
+
+import (
+	"context"
+	"database/sql"
+)
+
+
+type Storage struct {
+	Posts interface {
+		Create(context.Context) error
+	}
+	Users interface {
+		Create(context.Context) error
+	}
+
+}
+
+func NewPostgresStorage(db *sql.DB) Storage {
+	return Storage {
+		
+
+	}
+}
